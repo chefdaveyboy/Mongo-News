3d375f029015f363d84bcc753e532e4413587024
David Koeller <david.koeller@gmail.com>
2020-04-18 09:46:46 -0500

    hopefully this works?

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //NPM PACKAGES
 const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
+
 const mongoose = require("mongoose");
 
 // require the databases
@@ -11,19 +10,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect("mongodb://localhost/Mongo-News", {useNewUrlParser: true});
 // USE WHEN DEPLOYED TO HEROKU
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Mongo-News";
 
 mongoose.connect(MONGODB_URI);
 
-
+mongoose.set("useFindandModify", false);
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/Mongo-News", {useNewUrlParser: true});
+
 
 
 
